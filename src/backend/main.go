3f56a1c7c8b7b1a98d8d1b05912f1a2e30c9c08b
AlbertChoe <albert.choe73@gmail.com>
2024-04-22 14:35:03 +0700

    fix: handle api input is link not title

--- src/backend/main.go
+++ src/backend/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/url"
-	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -12,8 +10,8 @@ import (
 )
 
 type Input struct {
-	StartTitle string `json:"StartTitle"`
-	GoalTitle  string `json:"GoalTitle"`
+	StartTitleLink string `json:"StartTitleLink"`
+	GoalTitleLink  string `json:"GoalTitleLink"`
 	AlgoChoice int    `json:"AlgoChoice"`
 }
 
@@ -49,29 +47,26 @@ func postInformation(c *gin.Context) {
 func processInput(input Input) Result {
 	var err error
 
-	var starting_title_link string = titleToUrl(input.StartTitle);
-	var goal_title_link string = titleToUrl(input.GoalTitle);
-	fmt.Println(goal_title_link)
 	switch input.AlgoChoice {
 	case 1:
 		start := time.Now()
-		result, err, num_checked := IDS(starting_title_link, goal_title_link)
+		result, err, num_checked := IDS(input.StartTitleLink, input.GoalTitleLink)
 		elapsed := time.Since(start).Milliseconds()
 		if err == nil && result != nil {
 			return Result{
 				Status:      "Success",
-				Message:     fmt.Sprintf("Processed using Algo IDS with StartTitle: %s and GoalTitle: %s", starting_title_link, goal_title_link),
+				Message:     fmt.Sprintf("Processed using Algo IDS with StartTitle: %s and GoalTitle: %s", input.StartTitleLink, input.GoalTitleLink),
 				ShortestPath: result,
 				ExecTime: elapsed,
 				NumChecked: num_checked,
 			}
 		}
 	case 2:
-		result, _, num_checked, _, duration, err := bfs(starting_title_link, goal_title_link)
+		result, _, num_checked, _, duration, err := bfs(input.StartTitleLink, input.GoalTitleLink)
 		if err == nil && result != nil {
 			return Result{
 				Status:      "Success",
-				Message:     fmt.Sprintf("Processed using Algo 2 with StartTitle: %s and GoalTitle: %s", starting_title_link, goal_title_link),
+				Message:     fmt.Sprintf("Processed using Algo 2 with StartTitle: %s and GoalTitle: %s", input.StartTitleLink, input.GoalTitleLink),
 				ShortestPath: result,
 				ExecTime: duration,
 				NumChecked: num_checked,
@@ -85,9 +80,4 @@ func processInput(input Input) Result {
 		return Result{Status: "Error", Message: fmt.Sprintf("Error processing request: %v", err)}
 	}
 	return Result{Status: "Error", Message: "Path not found"}
-}
-
-func titleToUrl(title string) string {
-    escapedTitle := url.QueryEscape(strings.ReplaceAll(title, " ", "_"))
-    return "https://en.wikipedia.org/wiki/" + escapedTitle
 }
