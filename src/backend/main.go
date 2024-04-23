f2da78841568739b21d1d2bb8c663802b8a92bbf
ChaiGans <elbertchailes888@gmail.com>
2024-04-23 22:43:30 +0700

    feat: combine multipath and singlepath to one

--- src/backend/main.go
+++ src/backend/main.go
@@ -18,15 +18,7 @@ type Input struct {
 type Result struct {
 	Status      string   `json:"status"`
 	Message     string   `json:"message"`
-	ShortestPath []string `json:"shortestPath,omitempty"`
-	ExecTime int64 `json:"exectime,omitempty"`
-	NumChecked int `json:"numchecked,omitempty"`
-}
-
-type MultiResult struct {
-	Status      string   `json:"status"`
-	Message     string   `json:"message"`
-	PossibleShortestPath [][]string `json:"possibleShortestPath,omitempty"`
+	ShortestPath [][]string `json:"shortestPath,omitempty"`
 	ExecTime int64 `json:"exectime,omitempty"`
 	NumChecked int `json:"numchecked,omitempty"`
 }
@@ -35,7 +27,6 @@ func main() {
 	router := gin.Default()
 	router.Use(cors.Default())
 	router.POST("/", postInformation)
-	router.POST("/multi-api", postInformationMulti)
 	router.Run(":8080")
 }
 
@@ -47,54 +38,66 @@ func postInformation(c *gin.Context) {
 		return
 	}
 
-	result := processInputSingle(userInput)
+	result := processInput(userInput)
 
 	// Return the result as JSON.
 	c.JSON(http.StatusOK, result)
 }
 
-func postInformationMulti(c *gin.Context) {
-	var userInput Input
-
-	if err := c.BindJSON(&userInput); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
-		return
-	}
-
-	result := processInputMultiple(userInput)
-
-	// Return the result as JSON.
-	c.JSON(http.StatusOK, result)
-}
-
-func processInputSingle(input Input) Result {
+func processInput(input Input) Result {
 	var err error
+	var array_result [][]string
 
 	switch input.AlgoChoice {
 	case 1:
 		start := time.Now()
 		result, err, num_checked := IDS(input.StartTitleLink, input.GoalTitleLink)
+		array_result = append([][]string{}, result)
 		elapsed := time.Since(start).Milliseconds()
 		if err == nil && result != nil {
 			return Result{
 				Status:      "Success",
 				Message:     fmt.Sprintf("Processed using Algo IDS with StartTitle: %s and GoalTitle: %s", input.StartTitleLink, input.GoalTitleLink),
-				ShortestPath: result,
+				ShortestPath: array_result,
 				ExecTime: elapsed,
 				NumChecked: num_checked,
 			}
 		}
 	case 2:
 		result, _, num_checked, _, duration, err := bfs(input.StartTitleLink, input.GoalTitleLink)
+		array_result = append([][]string{}, result)
 		if err == nil && result != nil {
 			return Result{
 				Status:      "Success",
 				Message:     fmt.Sprintf("Processed using Algo 2 with StartTitle: %s and GoalTitle: %s", input.StartTitleLink, input.GoalTitleLink),
-				ShortestPath: result,
+				ShortestPath: array_result,
 				ExecTime: duration,
 				NumChecked: num_checked,
 			}
 		}
+	case 3:
+		result, err, num_checked, exectime := MultiIDS(input.StartTitleLink, input.GoalTitleLink)
+		if err == nil && result != nil {
+			return Result{
+				Status:      "Success",
+				Message:     fmt.Sprintf("Processed using Algo Multi-IDS with StartTitle: %s and GoalTitle: %s", input.StartTitleLink, input.GoalTitleLink),
+				ShortestPath: result,
+				ExecTime: exectime,
+				NumChecked: num_checked,
+			}
+		}
+	case 4:
+		result, err, num_checked, exectime := MultiIDS(input.StartTitleLink, input.GoalTitleLink)
+		if err == nil && result != nil {
+			return Result{
+				Status:      "Success",
+				Message:     fmt.Sprintf("Processed using Algo Multi-BFS with StartTitle: %s and GoalTitle: %s", input.StartTitleLink, input.GoalTitleLink),
+				ShortestPath: result,
+				ExecTime: exectime,
+				NumChecked: num_checked,
+			}
+		}
+
 	default:
 		return Result{Status: "Error", Message: "Invalid algorithm choice"}
 	}
@@ -103,40 +106,4 @@ func processInputSingle(input Input) Result {
 		return Result{Status: "Error", Message: fmt.Sprintf("Error processing request: %v", err)}
 	}
 	return Result{Status: "Error", Message: "Path not found"}
-}
-
-func processInputMultiple(input Input) MultiResult {
-	var err error
-
-	switch input.AlgoChoice {
-	case 1:
-		result, err, num_checked, exectime := MultiIDS(input.StartTitleLink, input.GoalTitleLink)
-		if err == nil && result != nil {
-			return MultiResult{
-				Status:      "Success",
-				Message:     fmt.Sprintf("Processed using Algo IDS with StartTitle: %s and GoalTitle: %s", input.StartTitleLink, input.GoalTitleLink),
-				PossibleShortestPath: result,
-				ExecTime: exectime,
-				NumChecked: num_checked,
-			}
-		}
-	case 2:
-		result, err, num_checked, exectime := MultiIDS(input.StartTitleLink, input.GoalTitleLink)
-		if err == nil && result != nil {
-			return MultiResult{
-				Status:      "Success",
-				Message:     fmt.Sprintf("Processed using Algo 2 with StartTitle: %s and GoalTitle: %s", input.StartTitleLink, input.GoalTitleLink),
-				PossibleShortestPath: result,
-				ExecTime: exectime,
-				NumChecked: num_checked,
-			}
-		}
-	default:
-		return MultiResult{Status: "Error", Message: "Invalid algorithm choice"}
-	}
-
-	if err != nil {
-		return MultiResult{Status: "Error", Message: fmt.Sprintf("Error processing request: %v", err)}
-	}
-	return MultiResult{Status: "Error", Message: "Path not found"}
 }
